b9c1cdce5217ecaaf1f5dd2e4d674f00786d276d
pritamdhar <pritamdhar@gmail.com>
2014-05-11 00:11:35 +0530

    Adding Plot 2 files and updated Plot 1 file
    Adding Plot 2 files and updated Plot 1 file

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-##File to create the plot1.png
+##File to create the plot2.png
 ##Assuming the data file is in the same directory as the code
 
 #Vector representing the column header data type
@@ -10,11 +10,12 @@ myData <- read.table('household_power_consumption.txt', sep=';', header=TRUE, co
 myData$DateTime <- strptime(paste(myData$Date, myData$Time), "%d/%m/%Y %H:%M:%S")
 myData <- subset(myData, as.Date(DateTime) >= as.Date("2007-02-01") & as.Date(DateTime) <= as.Date("2007-02-02"))
 
-# Create plot1.png
-png("plot1.png", height=480, width=480)
+# Create plot2.png
+png("plot2.png", height=480, width=480)
 
-# Build histogram
-hist(myData$Global_active_power, col='red', xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power')
+# Build the graph
+plot(myData$DateTime, myData$Global_active_power, pch=NA, xlab="", ylab="Global Active Power (kilowatts)")
+lines(myData$DateTime, myData$Global_active_power)
 
 # Close PNG file
 dev.off()
